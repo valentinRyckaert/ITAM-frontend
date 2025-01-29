2506003c0758e13669e2d22d1d8efc77bde69264
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-01-29 20:30:23 +0100

    add button for creating devices

--- src/lib/views/Devices.svelte
+++ src/lib/views/Devices.svelte
@@ -1,41 +1,51 @@
 <script lang="ts">
     import DataTable from '../components/DataTable.svelte'
-  import PostModal from '../components/modals/PostModal.svelte';
+    import PostModal from '../components/modals/PostModal.svelte'
 
-    let showModal = $state(false)
+    let showDeviceModal = $state(false)
+    let showGroupModal = $state(false)
     let dataForModal = $state({})
 
-    function activatePostModal(element: object) {
-      dataForModal = element
-      showModal = true
+    function activatePostModal(element: object, ressource: string) {
+    	dataForModal = element
+    	if(ressource == 'devices') showDeviceModal = true
+		else showGroupModal = true
     }
 </script>
 
 <div class="container">
     <h1>Gestion des Actifs</h1>
     
-    
     <h2>Actifs</h2>
-    <DataTable dataName="devices" config={{
-      DEV_id: '#',
-      DEV_name: 'name',
-      DEV_os: 'os',
-      DG_id: 'group id'
+    
+	<button onclick={() => activatePostModal({
+		DEV_id: ['id','number'],
+		DEV_name: ['name','string'],
+		DEV_os: ['os', 'string'],
+		DG_id: ['group id', 'number']
+    }, 'devices')}>create</button>
+	
+	<DataTable dataName="devices" config={{
+		DEV_id: '#',
+		DEV_name: 'name',
+		DEV_os: 'os',
+		DG_id: 'group id'
     }}/>
+	<PostModal bind:showModal={showDeviceModal} dataName="devices" objectToSend={dataForModal}/>
 
   
     <h2>Groupes d'Actifs</h2>
 
     <button onclick={() => activatePostModal({
-      DG_id: ['id','number'],
-      DG_libelle: ['libellé','string']
-    })}>create</button>
+		DG_id: ['id','number'],
+		DG_libelle: ['libellé','string']
+    }, 'groups')}>create</button>
 
     <DataTable dataName="devicegroups" config={{
-      DG_id: '#',
-      DG_libelle: 'libellé',
+		DG_id: '#',
+		DG_libelle: 'libellé',
     }}/>
-    <PostModal bind:showModal={showModal} dataName="devicegroups" objectToSend={dataForModal}/>
+    <PostModal bind:showModal={showGroupModal} dataName="devicegroups" objectToSend={dataForModal}/>
 </div>
 
 <style lang="scss">
