082ca69e058b427bfcbb6edaaa17d8d454c312c0
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-02-03 16:59:36 +0100

    improve home page

--- src/lib/views/Home.svelte
+++ src/lib/views/Home.svelte
@@ -1,28 +1,63 @@
 <script lang="ts">
-    import DataTable from '../components/DataTable.svelte'
-    import { getCurrentUser } from '../../api/auth'
+    import { onMount } from 'svelte'
+    import { getAllFromDB } from '../../api/data'
 
-    let currentUsername: string
+    let devices: { [key: string]: number } = {}
 
-    function getUser() {
-        const user = getCurrentUser()
-        currentUsername = user["username"]
-    }
+    onMount(async () => {
+        const data = await getAllFromDB("devices")
+        data.forEach(element => {
+            const os = element.DEV_os
+            if (os in devices) devices[os]++
+            else devices[os] = 1
+        })
+    })
 
 </script>
 
-<h2>Welcome {currentUsername}</h2>
-<DataTable dataName="devices" config={{
-    DEV_id: '#',
-    DEV_name: 'name',
-    DEV_os: 'os',
-    DG_id: 'group id'
-}}/>
+<h2>Welcome</h2>
+<div class="card-container">
+    {#each Object.entries(devices) as [key, value]}
+        <div class="card">
+            <div class="label">{key}</div>
+            <div class="number">{value}</div>
+        </div>
+    {/each}
+</div>
 
 <style lang="scss">
     h2 {
         text-align: center;
         color: #333;
         margin-bottom: 30px;
+    }
+
+    .card-container {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        padding: 20px;
+    }
+
+    .card {
+        background-color: #f9f9f9;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        text-align: center;
+        width: 200px;
+        margin: 20px auto;
+
+        .label {
+            font-size: 16px;
+            color: #555;
+            margin-bottom: 10px;
+        }
+
+        .number {
+            font-size: 48px;
+            font-weight: bold;
+            color: #2c3e50;
+        }
     }
 </style>
