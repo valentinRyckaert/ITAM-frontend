2caa7008b1adadc85a61cd0fcd8d7e7a8ecf6f73
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-02-12 13:21:01 +0100

    add account view

--- src/lib/views/Account.svelte
+++ src/lib/views/Account.svelte
@@ -1,1 +1,36 @@
+<script lang="ts">
+    import { getCurrentUser } from '../../api/auth'
+
+    let currentUser: any
+
+    async function getUser() {
+        currentUser = await getCurrentUser()
+    }
+
+</script>
+
 <h1>Account</h1>
+
+{#await getUser()}
+    <p>Loading user information...</p>
+{:then}
+    <div class="user-summary">
+        <h2>Resumé : {currentUser.USER_username}</h2>
+        <p>password : ****** <button>changer</button></p>
+        <p>is Active: {currentUser.USER_isActive}</p>
+    </div>
+{/await}
+
+<style lang="scss">
+    .user-summary {
+        background-color: #f9f9f9;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    }
+
+    p {
+        font-size: 1.1em;
+        margin: 5px 0;
+    }
+</style>
