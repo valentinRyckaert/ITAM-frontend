723967ae047168c43544a5ebcfd1ee960de962cf
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-04-21 13:09:52 +0200

    Feat: add icons on DataTable buttons

--- src/lib/components/data/DataTable.svelte
+++ src/lib/components/data/DataTable.svelte
@@ -76,7 +76,11 @@
     <h1>loading...</h1>
 {:then} 
   {#if canCreate}
-    <button class="btn btn-create" onclick={() => activateModal(createOrUpdateConfig, {}, 'post')}>add</button>
+    <button class="btn btn-create" onclick={() => activateModal(createOrUpdateConfig, {}, 'post')}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg icon" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+      </svg>
+    </button>
   {/if}
     <table>
         <thead>
@@ -102,10 +106,25 @@
                       {/if}
                     {/each}
                     <td>
-                      <button class="btn btn-read" onclick={() => activateModal(showOrDeleteConfig, data[i], 'read')}>show</button>
+                      <button class="btn btn-read" onclick={() => activateModal(showOrDeleteConfig, data[i], 'read')}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye icon" viewBox="0 0 16 16">
+                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+                        </svg>
+                      </button>
                       {#if currentUser.USER_type < 2}
-                        <button class="btn btn-update" onclick={() => activateModal(createOrUpdateConfig, data[i], 'update')}>update</button>
-                        <button class="btn btn-delete" onclick={() => activateModal(showOrDeleteConfig, data[i], 'delete')}>delete</button>
+                        <button class="btn btn-update" onclick={() => activateModal(createOrUpdateConfig, data[i], 'update')}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square icon" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                          </svg>
+                        </button>
+                        <button class="btn btn-delete" onclick={() => activateModal(showOrDeleteConfig, data[i], 'delete')}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash icon" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                        </button>
                       {/if}
                     </td>
                 </tr>
@@ -143,21 +162,30 @@
     }
 
     .btn {
-      border-radius: 10px;
+      border-radius: 7px;
       border-color: transparent;
       color: white;
+      margin-block: 7px;
+      padding-block: 2px;
+      padding-inline: 10px;
       &-create {
         margin-bottom: 10px;
         background-color: rgb(39, 151, 39);
       }
       &-read {
-        background-color: rgb(78, 115, 163);
+        background-color: rgb(42, 108, 194);
       }
       &-update {
-        background-color: rgb(187, 107, 15);
+        background-color: rgb(202, 111, 6);
       }
       &-delete {
         background-color: rgb(204, 34, 34);
       }
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
 </style>
